1ee740c3d03a26df3d692598c9babb1e154ef880
djteiji <djteiji@gmail.com>
2022-04-07 23:50:23 -0700

    add msg and link if more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 
 
@@ -11,6 +12,12 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
 
@@ -74,7 +81,21 @@ var displayIssues = function(issues) {
     }
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+
+    linkEl.textContent = "See More Issues on Github.com";
+
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
 
 
-
-getRepoIssues("facebook/react");
+getRepoIssues("expressjs/express");
